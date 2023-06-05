0dfa981a524d0528ee854f233f6b6c50bd6e8fef
123-98 <pathakshripad98@gmail.com>
2023-06-05 15:50:59 +0530

    Test_Project_Changes

---
+++ src/test/java/Test_1/Demo_1.java
@@ -0,0 +1,20 @@
+package Test_1;
+
+import org.testng.annotations.Test;
+
+public class Demo_1 {
+	@Test
+	public void test1() {
+		System.out.println("Test 1 Run successfully");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Test 2 Run successfully");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("Test 3 Run successfully");
+	}
+}
